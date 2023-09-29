4fb798d642e191712ad925e515d42bf69e07aed8
Dame <mboup124@gmail.com>
2023-09-29 18:20:21 +0200

    Add image person

--- app.py
+++ app.py
@@ -18,14 +18,16 @@ def index():
         firstName = request.form['firstName']
         name = request.form['name']
         email = request.form['email']
+        image = request.form['image']
         cur = mysql.connection.cursor()
-        cur.execute('''CREATE TABLE IF NOT EXISTS flask_array (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                firstname VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
-                name VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
-                email VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci
-            );''')
-        cur.execute("INSERT INTO flask_array (firstName, name, email) VALUES (%s, %s, %s)", (firstName, name, email))
+        # cur.execute('''CREATE TABLE IF NOT EXISTS flask_array (
+        #         id INT AUTO_INCREMENT PRIMARY KEY,
+        #         firstname VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
+        #         name VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
+        #         email VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci
+        #         image VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci
+        #     );''')
+        cur.execute("INSERT INTO flask_array (firstName, name, email, image) VALUES (%s, %s, %s, %s)", (firstName, name, email, image))
         mysql.connection.commit()
         cur.close()
         return redirect("/")
@@ -34,7 +36,7 @@ def index():
         personsIndex = cur.execute("SELECT * FROM flask_array")
         personsIndex = cur.fetchall()
         cur.close()
-        personsIndex = [dict(id=row[0], firstName=row[1], name=row[2], email=row[3], created_at=row[4]) for row in personsIndex]
+        personsIndex = [dict(id=row[0], firstName=row[1], name=row[2], email=row[3], created_at=row[4], image=row[5]) for row in personsIndex]
     return render_template("index.html", personsIndex=personsIndex)
 
 # Mettre à jour une personne
@@ -44,10 +46,11 @@ def update(id):
         firstName = request.form['firstName']
         name = request.form['name']
         email = request.form['email']
+        image = request.form['image']
         cur = mysql.connection.cursor()
         cur.execute(
-            "UPDATE flask_array SET firstName = %s, name = %s, email = %s WHERE id = %s",
-            (firstName, name, email, id)
+            "UPDATE flask_array SET firstName = %s, name = %s, email = %s, image = %s WHERE id = %s",
+            (firstName, name, email, image, id)
         )
         mysql.connection.commit()
         cur.close()
@@ -60,36 +63,20 @@ def update(id):
         cur.close()
         if person is None:
             return "Record not found", 404
-        person_dict = dict(id=person[0], firstName=person[1], name=person[2], email=person[3], created_at=person[4])
+        person_dict = dict(id=person[0], firstName=person[1], name=person[2], email=person[3], created_at=person[4], image=person[5])
         return render_template("update.html", person=person_dict)
 
 # Suprrimer une personne
-@app.route("/delete/<int:id>/", methods=["GET", "POST"])
+@app.route("/delete/<int:id>/")
 def delete(id):
-    cur = mysql.connection.cursor()
-    #Partie optionnelle qui permet de passer le prénom et le nom du la personne concernée à delete_confirmation.html
-    cur.execute("SELECT * FROM flask_array WHERE id = %s", (id,))
-    person = cur.fetchone()
-    cur.close()
-
-    if person is None:
-        return "Record not found", 404
-
-    person_dict = dict(id=person[0], firstName=person[1], name=person[2], email=person[3], created_at=person[4])
-
-    if request.method == "POST":
-        try:
-            cur = mysql.connection.cursor()
-            cur.execute('DELETE FROM flask_array WHERE id = %s', [id])
-            mysql.connection.commit()
-            cur.close()
-            return redirect("/")
-        except Exception:
-            return "Une erreur s'est produite"
-    else:
-        return render_template("delete_confirmation.html", person=person_dict)
-
-
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute('DELETE FROM flask_array WHERE id = %s', [id])
+        mysql.connection.commit()
+        cur.close()
+        return redirect("/")
+    except Exception:
+        return "Une erreur s'est produite"
 
 # A propos de ...
 @app.route('/about/')
@@ -119,3 +106,35 @@ if __name__ == '__main__':
 #     #         );''')
 #     mysql.connection.commit()
 #     cur.close()
+
+
+# Suprrimer d'une personne personne avec confirmation
+# @app.route("/delete/<int:id>/", methods=["GET", "POST"])
+# def delete(id):
+#     cur = mysql.connection.cursor()
+#     #Partie optionnelle qui permet de passer le prénom et le nom du la personne concernée à delete_confirmation.html
+#     cur.execute("SELECT * FROM flask_array WHERE id = %s", (id,))
+#     person = cur.fetchone()
+#     cur.close()
+
+#     if person is None:
+#         return "Record not found", 404
+
+#     person_dict = dict(id=person[0], firstName=person[1], name=person[2], email=person[3], created_at=person[4], image=person[3])
+
+#     if request.method == "POST":
+#         try:
+#             cur = mysql.connection.cursor()
+#             cur.execute('DELETE FROM flask_array WHERE id = %s', [id])
+#             mysql.connection.commit()
+#             cur.close()
+#             return redirect("/")
+#         except Exception:
+#             return "Une erreur s'est produite"
+#     else:
+#         return render_template("delete_confirmation.html", person=person_dict)
+
+# persons = [
+#     {'id': 1, 'name': 'Alice', 'image_filename': 'alice.jpg'},
+#     {'id': 2, 'name': 'Bob', 'image_filename': 'bob.jpg'}
+# ]
